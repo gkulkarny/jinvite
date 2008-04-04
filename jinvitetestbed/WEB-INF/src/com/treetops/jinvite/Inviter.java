535c6f4d8a1a8b0dc6bf18201634ee9d14478412
chasd00 <chasd00@8d626295-4c49-0410-acfb-394a87e4f62c>
2008-04-04 01:11:19 +0000

    Version 0.1.0 commit. All unit tests pass - Chad

--- jinvitetestbed/WEB-INF/src/com/treetops/jinvite/Inviter.java
+++ jinvitetestbed/WEB-INF/src/com/treetops/jinvite/Inviter.java
@@ -5,33 +5,28 @@ import java.util.UUID;
 import com.treetops.jinvite.dao.InviteDAO;
 import com.treetops.jinvite.pojo.Invite;
 
-public final class Inviter {
+public class Inviter {
 
-	Connection conn = null;
-	String user = null;
-	
-	public Inviter( String user, Connection conn ) {
-		this.conn = conn;
-		this.user = user;
-	}
-	
-	public String getInviteCode() {
+	public static String getInviteCode(Connection conn, String user) {
 		String code = null;
 
-		InviteDAO inviteDAO = new InviteDAO(conn);
-		do {
-			code = UUID.randomUUID().toString();
-		} while ( inviteDAO.codeExists(code) );
-		Invite invite = new Invite();
-		invite.setCode(code);
-		invite.setUser(user);
-		inviteDAO.save(invite);
+		// user must be set, leave the code set to null if this class was mistakenly constructed without a user argument
+		if ( user != null ) {
+			InviteDAO inviteDAO = new InviteDAO(conn);
+			do {
+				code = UUID.randomUUID().toString();
+			} while ( inviteDAO.codeExists(code) );
+			Invite invite = new Invite();
+			invite.setCode(code);
+			invite.setUser(user);
+			inviteDAO.save(invite);
+		}
 	
 		return code;
 		
 	}
 
-	public boolean confirm( String code ) {
+	public static boolean confirm( Connection conn, String code ) {
 		boolean confirmed = false;
 		
 		InviteDAO inviteDAO = new InviteDAO(conn);
